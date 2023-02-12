cea366e38a5c4a924803f5f42cea0183f2e5940c
andrea-green <108085381+andrea-green@users.noreply.github.com>
2023-02-11 17:10:50 -0700

    about styling

--- package/tonni/.next/static/chunks/webpack.js
+++ package/tonni/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9d2de55dcd8ade90"; }
+/******/ 		__webpack_require__.h = function() { return "a02b9e68430fd83b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- package/tonni/src/components/About.js
+++ package/tonni/src/components/About.js
@@ -34,73 +34,7 @@ const About = () => {
                 I love seeking out new challenges and opportunities to improve my skills, and
                 am driven by my love of learning and my passion for this field,
                 which I see as having endless opportunities for growth and development.
-
                 </p>
-              </div>
-              <div className="dodo_progress">
-                <ul>
-                  <li>
-                    <div className="list_inner">
-                      <div
-                        className="progress_inner skillsInner___"
-                        data-value={90}
-                        data-color="#000"
-                      >
-                        <div className="background">
-                          <div className="bar">
-                            <div className="bar_in" />
-                          </div>
-                        </div>
-                        <div className="percent">
-                          <img
-                            className="svg"
-                            src="img/svg/photoshop.svg"
-                            alt=""
-                          />
-                          <span className="number">90%</span>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="list_inner">
-                      <div
-                        className="progress_inner skillsInner___"
-                        data-value={75}
-                        data-color="#0ACF83"
-                      >
-                        <div className="background">
-                          <div className="bar">
-                            <div className="bar_in" />
-                          </div>
-                        </div>
-                        <div className="percent">
-                          <img className="svg" src="img/svg/xd.svg" alt="" />
-                          <span className="number">75%</span>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="list_inner">
-                      <div
-                        className="progress_inner skillsInner___"
-                        data-value={80}
-                        data-color="#f75023"
-                      >
-                        <div className="background">
-                          <div className="bar">
-                            <div className="bar_in" />
-                          </div>
-                        </div>
-                        <div className="percent">
-                          <img className="svg" src="img/svg/figma.svg" alt="" />
-                          <span className="number">80%</span>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
               </div>
             </div>
           </div>
